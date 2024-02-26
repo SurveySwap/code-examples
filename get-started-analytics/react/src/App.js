a6139d479c6aed60b360bfa0c99ba7b04dd0e9af
Cuong Nguyen <npcuong.011308@gmail.com>
2024-02-26 16:46:07 +0700

    Use lib from CDN

--- get-started-analytics/react/src/App.js
+++ get-started-analytics/react/src/App.js
@@ -1,81 +1,49 @@
-import './App.css'
+import "./App.css";
 
-import { useState, useEffect } from 'react';
-import 'survey-analytics/survey.analytics.min.css';
-import { Model } from 'survey-core';
-import { VisualizationPanel } from 'survey-analytics';
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
-const surveyJson = {
-  elements: [{
-    name: "satisfaction-score",
-    title: "How would you describe your experience with our product?",
-    type: "radiogroup",
-    choices: [
-      { value: 5, text: "Fully satisfying" },
-      { value: 4, text: "Generally satisfying" },
-      { value: 3, text: "Neutral" },
-      { value: 2, text: "Rather unsatisfying" },
-      { value: 1, text: "Not satisfying at all" }
-    ],
-    isRequired: true
-  }, {
-    name: "nps-score",
-    title: "On a scale of zero to ten, how likely are you to recommend our product to a friend or colleague?",
-    type: "rating",
-    rateMin: 0,
-    rateMax: 10,
-  }],
-  showQuestionNumbers: "off",
-  completedHtml: "Thank you for your feedback!",
+const loadScript = function (url) {
+  const script = document.createElement("script");
+
+  script.src = url;
+  script.async = false;
+
+  document.body.appendChild(script);
+
+  return script;
 };
 
-const surveyResults = [{
-  "satisfaction-score": 5,
-  "nps-score": 10
-}, {
-  "satisfaction-score": 5,
-  "nps-score": 9
-}, {
-  "satisfaction-score": 3,
-  "nps-score": 6
-}, {
-  "satisfaction-score": 3,
-  "nps-score": 6
-}, {
-  "satisfaction-score": 2,
-  "nps-score": 3
-}];
-
-const vizPanelOptions = {
-  allowHideQuestions: false
-}
-
 export default function App() {
-  const [survey, setSurvey] = useState(null);
-  const [vizPanel, setVizPanel] = useState(null);
-  if (!survey) {
-    const survey = new Model(surveyJson);
-    setSurvey(survey);
-  }
-
-  if (!vizPanel && !!survey) {
-    const vizPanel = new VisualizationPanel(
-      survey.getAllQuestions(),
-      surveyResults,
-      vizPanelOptions
-    );
-    vizPanel.showHeader = false;
-    setVizPanel(vizPanel);
-  }
-
   useEffect(() => {
-    vizPanel.render(document.getElementById("surveyVizPanel"));
+    const scripts = [
+      loadScript("https://unpkg.com/survey-core/survey.core.min.js"),
+      loadScript("https://cdn.plot.ly/plotly-latest.min.js"),
+      loadScript("https://unpkg.com/wordcloud/src/wordcloud2.js"),
+      loadScript("https://unpkg.com/survey-analytics/survey.analytics.min.js"),
+      loadScript("./analytics.js"),
+    ];
+
     return () => {
-      document.getElementById("surveyVizPanel").innerHTML = "";
-    }
-  }, [vizPanel]);
+      scripts.forEach((script) => document.body.removeChild(script));
+    };
+  }, []);
 
   return (
-    <div id="surveyVizPanel" />
+    <>
+      <Helmet>
+        <link
+          href="https://unpkg.com/survey-analytics/survey.analytics.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://unpkg.com/survey-core/defaultV2.min.css"
+          type="text/css"
+          rel="stylesheet"
+        />
+      </Helmet>
+
+      <div id="surveyVizPanel"></div>
+    </>
   );
 }
